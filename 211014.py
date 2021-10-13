8b40f6c3cfb936de1107728cac0eb533e376a924
jadeplanet <seychelle86@gmail.com>
2021-10-14 02:10:29 +0900

    baekjoon : 17219

---
+++ 211014.py
@@ -0,0 +1,10 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+n, m = map(int, input().split())
+arr = dict(input().split() for _ in range(n))
+
+for _ in range(m):
+  site = input()
+  print(arr.get(site))
