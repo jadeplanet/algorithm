34561aa303afabccb609cc6f30226e17a4518220
jadeplanet <seychelle86@gmail.com>
2021-10-13 22:13:41 +0900

    baekjoon : 14425

---
+++ 211013.py
@@ -0,0 +1,9 @@
+from sys import stdin
+
+N, M = map(int, stdin.readline().split())
+
+target = [stdin.readline().strip() for _ in range(N)]
+container = [stdin.readline().strip() for _ in range(M)]
+result = set([item for item in container if item in target])
+
+print(len(result))
--- example.py
+++
@@ -1,2 +0,0 @@
-print("hello world!")
-print("this is repository for algorithm test.")
