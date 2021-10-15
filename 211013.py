1e35fc73a74357e609cbd92489fe9de8716847fe
jadeplanet <seychelle86@gmail.com>
2021-10-15 11:09:13 +0900

    baekjoon : 14425 문자열 집합

--- 211013.py
+++ 211013.py
@@ -4,6 +4,8 @@ N, M = map(int, stdin.readline().split())
 
 target = [stdin.readline().strip() for _ in range(N)]
 container = [stdin.readline().strip() for _ in range(M)]
+
+# 주의: 고유한 값을 남기기 위해 set 함수를 사용하면 정답이 나오지 않음
 result = [item for item in container if item in target]
 
 print(len(result))
