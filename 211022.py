63009e7f413f787b875e8213c0a0c7fb4f96e3cc
jadeplanet <seychelle86@gmail.com>
2021-10-23 15:47:19 +0900

    baekjoon : 1065 한수

--- 211022.py
+++ 211022.py
@@ -9,7 +9,7 @@ else:
     # 100 이상의 자연수에서 한수 찾기
     count = 99
     for i in range(100, n+1):
-        i = str(i) # 숫자를 문자열로 형변환
+        i = str(i) # 숫자형을 문자형으로 타입 변환
         digits = [int(item) for item in list(i)] # 문자열을 한 글자씩 나눠서 리스트에 담고, 각 원소를 숫자형으로 변환
         if digits[0] - digits[1] == digits[1] - digits[2]:  # 각 자릿수의 등차수열 여부 확인
             count += 1
